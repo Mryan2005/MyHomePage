d6be4f1be7fbe7d29ce991c1d04fada6ed34e061
Yan Zhizhong <a2564011261@163.com>
2025-06-08 10:54:31 +0800

    fix: 添加 PLATFORM_ID 注入以支持平台特定功能

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,32 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
-import { InsideWindow } from './components/inside-window/inside-window';
+import { AfterViewInit, Component, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { BackgroundService } from './config/background';
-import { Footbar } from './components/footbar/footbar';
 
 @Component({
   selector: 'app-root',
-  imports: [InsideWindow, Footbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements AfterViewInit {
-    constructor(
-      public bgService: BackgroundService,
-      public elementRef: ElementRef
-    ) {}
+  constructor(
+    public bgService: BackgroundService,
+    public elementRef: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
-    protected title = 'myhomeIndex';
-    
-    ngAfterViewInit(): void {
-        const mainElement = document.querySelector('body') as HTMLElement;
-        if (mainElement) {
-            mainElement.style.transition = 'background-image 1s ease-in-out';
-            mainElement.style.backgroundImage = `url('${this.bgService.getBackgroundImage()}')`;
-            mainElement.style.backgroundSize = 'cover';
-            mainElement.style.backgroundPosition = 'center';
-            mainElement.style.backgroundRepeat = 'no-repeat';
-            mainElement.style.backgroundAttachment = 'fixed';
-        }
+  protected title = 'myhomeIndex';
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const mainElement = document.querySelector('body') as HTMLElement;
+      if (mainElement) {
+        mainElement.style.transition = 'background-image 1s ease-in-out';
+        mainElement.style.backgroundImage = `url('${this.bgService.getBackgroundImage()}')`;
+        mainElement.style.backgroundSize = 'cover';
+        mainElement.style.backgroundPosition = 'center';
+        mainElement.style.backgroundRepeat = 'no-repeat';
+        mainElement.style.backgroundAttachment = 'fixed';
+      }
     }
+  }
 }
