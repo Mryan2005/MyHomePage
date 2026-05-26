4d8a6e7b022ef99f3cf60674c44910dbe5ea12c4
Yan Zhizhong <a2564011261@163.com>
2026-05-26 21:02:43 +0800

    feat: implement GitHub discussions integration and update related services

---
+++ scripts/generate-discussions.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+
+const token = process.env.GH_TOKEN;
+
+async function generate() {
+
+    const query = `
+{
+  repository(owner: "Mryan2005", name: "MyHomePage") {
+
+    discussions(
+      first: 10,
+      categoryId: "DIC_kwDOMuiJU84C94Ga"
+    ) {
+
+      nodes {
+
+        title
+
+        url
+
+        createdAt
+
+        closed
+
+        body
+
+        category {
+          name
+        }
+
+        author {
+          login
+        }
+
+      }
+
+    }
+
+  }
+
+}
+`;
+
+    const response = await fetch(
+        'https://api.github.com/graphql',
+        {
+            method: 'POST',
+
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+                query
+            })
+        }
+    );
+
+    const result = await response.json();
+
+    console.log(result);
+
+    const discussions =
+        result.data.repository.discussions.nodes
+            .filter(d => !d.closed)
+            .slice(0, 10);
+
+    fs.writeFileSync(
+        './src/assets/discussions.json',
+        JSON.stringify(discussions, null, 2)
+    );
+
+    console.log('discussions.json generated');
+}
+
+generate();
