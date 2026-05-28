7c9d7e2b2c4fd58f6faee48b3594c7eacd55627b
Yim Chichung <zhizhongyan@qq.com>
2026-05-28 10:37:57 +0800

    add Github and Email

--- src/app/data/contacts.ts
+++ src/app/data/contacts.ts
@@ -2,12 +2,13 @@ import { Contact } from "../interfaces/Contact";
 
 export const contactslist: Contact[] = [
     {
-        title: 'Email',
+        title: 'Email: mryan2005@mryan2005.top',
         url: 'mailto:mryan2005@mryan2005.top',
         valid: true
     },
     {
-        title: 'GitHub',
-        url: 'https://
+        title: 'GitHub: Mryan2005',
+        url: 'https://github.com/Mryan2005',
+        valid: true
     }
 ]
