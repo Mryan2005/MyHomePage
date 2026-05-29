e57a07f7b4a37fa294c51274f0d8a55b93c175ca
Yim Chichung <A2564011261@163.com>
2026-05-29 08:36:35 +0800

    Add multiple contact entries to contacts.ts

--- src/app/data/contacts.ts
+++ src/app/data/contacts.ts
@@ -10,5 +10,50 @@ export const contactslist: Contact[] = [
         title: 'GitHub: Mryan2005',
         url: 'https://github.com/Mryan2005',
         valid: true
+    },
+    {
+        title: 'Blog',
+        url: 'https://blog.mryan2005.top',
+        valid: true
+    },
+    {
+        title: 'CSDN: Mryan2005',
+        url: 'https://blog.csdn.net/qq_21739599',
+        valid: true
+    },
+    {
+        title: 'Akiacg Forum: 夜桜閑客',
+        url: 'https://forum.akiacg.com/u/Mryan2005',
+        valid: true
+    },
+    {
+        title: 'Gitee: Mryan2005',
+        url: 'https://gitee.com/Mryan2005',
+        valid: true
+    },
+    {
+        title: 'LeetCode: Mryan2005',
+        url: 'https://leetcode.cn/u/Mryan2005/',
+        valid: true
+    },
+    {
+        title: 'Lofter: Mryan2005',
+        url: 'http://mryan2005.lofter.com/',
+        valid: true
+    },
+    {
+        title: '蓝桥云课: Mryan2005',
+        url: 'https://www.lanqiao.cn/users/2902680/',
+        valid: true
+    },
+    {
+        title: 'X: Mryan2005_1',
+        url: 'https://x.com/Mryan2005_1',
+        valid: true
+    },
+    {
+        title: 'ORCID: 0009-0001-1431-9309',
+        url: 'https://orcid.org/0009-0001-1431-9309',
+        valid: true
     }
 ]
