641a0addcb45f3058dc576555cd5d4920ecf8cc4
Yim Chichung <zhizhongyan@qq.com>
2026-06-01 18:05:30 +0800

    add ciallo image

--- src/app/components/sub-introduce-myself-window/sub-introduce-myself-window.ts
+++ src/app/components/sub-introduce-myself-window/sub-introduce-myself-window.ts
@@ -14,9 +14,32 @@ import {PhotoViewerService} from '../../services/photo-viewer.service';
 export class SubIntroduceMyselfWindow implements OnInit {
     public currentDisplayPart: string = 'Home';
     public adam_smith_avatar: string = '/assets/images/7646049331687920481(20260531-213655).png'
+    public ciallo_image = ['/assets/images/ciallo/Murasame_ciallo.png', '/assets/images/ciallo/Yoshino_Ciallo.png'];
 
     openPortal() {
         this.websitePramas.currentDisplayPart = 'Works';
+    }
+
+    openCialloImage() {
+        const mean = (this.ciallo_image.length - 1) / 2;
+        const stdDev = this.ciallo_image.length / 5; // 控制集中程度（越小越集中）
+
+        let index = Math.round(gaussianRandom(mean, stdDev));
+
+        // 边界保护
+        index = Math.max(0, Math.min(this.ciallo_image.length - 1, index));
+
+        this.photoViewer.open(this.ciallo_image[index]);
+    }
+
+    gaussianRandom(mean = 0, stdDev = 1): number {
+        let u = 0, v = 0;
+
+        while (u === 0) u = Math.random();
+        while (v === 0) v = Math.random();
+
+        const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+        return z * stdDev + mean;
     }
 
     constructor(
